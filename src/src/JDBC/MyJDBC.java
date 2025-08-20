fe4d53ada3064cefbe2336577cbd989d17039c56
Heitor <srheitor167@gmail.com>
2025-08-20 18:50:39 -0300

    Menu choices and core package created.

---
+++ src/src/JDBC/MyJDBC.java
@@ -0,0 +1,34 @@
+package JDBC;
+
+import java.sql.*;
+
+public class MyJDBC {
+    public static void conect() {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/library_schema",
+                    "root",
+                    "989796"
+
+            );
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKS");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("idbooks"));
+                System.out.println(resultSet.getString("booksname"));
+                if(resultSet.getInt("bookstate") == 1){
+                    System.out.println("Em estoque");
+                }else{
+                    System.out.println("Emprestado");
+                }
+                System.out.println(resultSet.getString("booksgenre"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/src/core/Utils.java
@@ -0,0 +1,7 @@
+package core;
+
+import java.util.Scanner;
+
+public class Utils {
+    public static Scanner SCANNER = new Scanner(System.in);
+}
