efe21b9ab7a99d6d19bc0a3227168162aa4e29ea
Heitor <souzaheitor167@gmail.com>
2025-08-21 19:30:07 -0300

    feat(takeBook):função de retirar livro (incompleta)

--- src/src/JDBC/MyJDBC.java
+++ src/src/JDBC/MyJDBC.java
@@ -3,32 +3,20 @@ package JDBC;
 import java.sql.*;
 
 public class MyJDBC {
-    public static void conect() {
+    public static Connection connect() {
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     "jdbc:mysql://127.0.0.1:3306/library_schema",
                     "root",
                     "989796"
 
             );
 
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKS");
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("idbooks"));
-                System.out.println(resultSet.getString("booksname"));
-                if(resultSet.getInt("bookstate") == 1){
-                    System.out.println("Em estoque");
-                }else{
-                    System.out.println("Emprestado");
-                }
-                System.out.println(resultSet.getString("booksgenre"));
-
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        return connection;
     }
 }
--- src/src/app/Main.java
+++ src/src/app/Main.java
@@ -1,15 +1,28 @@
 package app;
 
+import JDBC.MyJDBC;
+import core.Utils;
+
+import java.sql.SQLException;
+
 import static core.Utils.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Olá, seja bem vindo a livraria de teste do Heitor! \n" +
                            "O que você deseja fazer?");
 
         int choice = Menu();
         switch (choice){
-            case 1 -> System.out.println("Você pegou um livro");
+            case 1 -> {
+                System.out.println("Selecione qual livro você deseja pegar (Digite o ID do livro): ");
+                Utils.showBooks();
+                int bookId = SCANNER.nextInt();
+                takeBook(bookId);
+
+
+            }
+
             case 2 -> System.out.println("Você devolveu um livro");
             case 3 -> System.out.println("Você adicionou um livro");
             case 4 -> System.out.println("Você saiu");
