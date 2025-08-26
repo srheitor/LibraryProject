a8ff9f7f2bf82ec9c0a205363625fc6efea7d62a
Heitor <srheitor167@gmail.com>
2025-08-26 18:01:39 -0300

    feat(takeBook); Feature concluded

--- src/src/app/Main.java
+++ src/src/app/Main.java
@@ -1,34 +1,44 @@
 package app;
 
 import core.Utils;
+
 import java.sql.SQLException;
+
 import static core.Utils.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("Olá, seja bem vindo a livraria de teste do Heitor! \n" +
-                           "O que você deseja fazer?");
+                "Por favor digite seu usuário e senha: ");
 
-        int choice = Menu();
-        switch (choice){
-            case 1 -> {
-                System.out.println("Selecione qual livro você deseja pegar (Digite o ID do livro): ");
-                Utils.showBooks();
-                int bookId = SCANNER.nextInt();
-                takeBook(bookId);
+        String user = SCANNER.nextLine();
+        int psw = SCANNER.nextInt();
+
+        if () {
+            System.out.println("Olá admin!, o que gostaria de fazer?");
+            int choice = AdminMenu();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Selecione qual livro você deseja pegar (Digite o ID do livro): ");
+                    Utils.showBooks();
+                    int bookId = SCANNER.nextInt();
+                    takeBook(bookId);
+
+                }
+
+                case 2 -> System.out.println("Você devolveu um livro");
+                case 3 -> System.out.println("Você adicionou um livro");
+                case 4 -> System.out.println("Você saiu");
 
             }
-
-            case 2 -> System.out.println("Você devolveu um livro");
-            case 3 -> System.out.println("Você adicionou um livro");
-            case 4 -> System.out.println("Você saiu");
-
+        } else if () {
+            System.out.println("Olá usuario!, o que gostaria de fazer?");
         }
 
-        //MyJDBC.conect();
 
     }
-    public static int Menu(){
+
+    public static int AdminMenu() {
         System.out.println("01. Pegar um livro");
         System.out.println("02. Devolver um livro");
         System.out.println("03. Adicionar um novo livro");
--- src/src/core/Utils.java
+++ src/src/core/Utils.java
@@ -1,42 +1,71 @@
 package core;
 
 import JDBC.MyJDBC;
+import core.dto.UserDTO;
 
 import java.sql.*;
 import java.util.Scanner;
 
 public class Utils {
     public static Scanner SCANNER = new Scanner(System.in);
+    public static int USER_TYPE_ADMIN = 0;
+    public static int USER_TYPE_USER = 1;
 
-    public static void showBooks() throws SQLException {
+    public static void showBooks(){
         String sql = "SELECT * FROM BOOKS";
 
-        Statement statement = MyJDBC.connector().createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
+        try {
+            Statement statement = MyJDBC.connector().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
 
-        while (resultSet.next()) {
-            System.out.println(resultSet.getInt("idbooks"));
-            System.out.println(resultSet.getString("bookname"));
-            if (resultSet.getInt("bookstate") == 1) {
-                System.out.println("Em estoque");
-            } else {
-                System.out.println("Emprestado");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("idbooks"));
+                System.out.println(resultSet.getString("bookname"));
+                if (resultSet.getInt("bookstate") == 1) {
+                    System.out.println("Em estoque");
+                } else {
+                    System.out.println("Emprestado");
+                }
+                System.out.println(resultSet.getString("booksgenre"));
+
             }
-            System.out.println(resultSet.getString("booksgenre"));
-
+        } catch (SQLException e) {
+            System.out.println("Não foi carregar a livraria :(");
         }
     }
-    public static void takeBook(int bookId) throws SQLException {
+
+    public static void takeBook(int bookId){
         String sql = "UPDATE books SET bookstate = ? WHERE IDBOOKS = ?";
-        Connection connection = MyJDBC.connector();
-        PreparedStatement pst;
+        try {
+            Connection connection = MyJDBC.connector();
+            PreparedStatement pst;
 
-        pst = connection.prepareStatement(sql);
+            pst = connection.prepareStatement(sql);
 
-        pst.setInt(1, 0);
-        pst.setInt(2, bookId);
+            pst.setInt(1, 0);
+            pst.setInt(2, bookId);
 
-        pst.executeUpdate();
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Não foi possível pegar o livro");
+        }
 
+    }
+
+    public static void login(int bookId) {
+        String sql = "SELECT * FROM users WHERE userName = ? and password = ?";
+        try {
+            Connection connection = MyJDBC.connector();
+            PreparedStatement pst;
+
+            pst = connection.prepareStatement(sql);
+
+            pst.setInt(1, UserDTO.getUserName());
+            pst.setInt(2, bookId);
+
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Não foi possível pegar o livro");
+        }
     }
 }
