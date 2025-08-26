8ad27639108c6fbafe1ca0da6c0d891d474f26b1
Heitor <srheitor167@gmail.com>
2025-08-26 18:07:26 -0300

    feat(takeBook); Feature concluded

---
+++ src/src/core/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package core.dto;
+
+public class UserDTO {
+    private int idUser;
+    private String userName;
+    private int password;
+    private int userType;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+}
